e407efb2078989dddd738ffcdeb316bbbde48892
Marian Stanciulica <marian.stanciulica@gmail.com>
2018-10-18 22:49:11 +0300

    Current Score Added

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,9 +8,11 @@ public class Block : MonoBehaviour {
 
     // cached reference
     Level level;
+    GameStatus gameStatus;
 
     private void Start()
     {
+        gameStatus = FindObjectOfType<GameStatus>();
         level = FindObjectOfType<Level>();
         level.CountBreakableBlocks();
     }
@@ -22,6 +24,7 @@ public class Block : MonoBehaviour {
 
     private void DestroyBlock()
     {
+        gameStatus.AddToScore();
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
         level.BlockDestroyed();
